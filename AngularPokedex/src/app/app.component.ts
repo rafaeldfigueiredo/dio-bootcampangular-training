bd53dafc8fd4f31af4f81509c8e53ea0f025af61
Rafael 'Fig' Diamantino de Figueiredo <45023203+rafaeldfigueiredo@users.noreply.github.com>
2023-12-20 23:50:02 +0000

    add angular workspace

---
+++ AngularPokedex/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  constructor() { }
+  nome:string = '';
+  ngOnInit() {
+    this.nome = 'Angular';
+  }
+}
