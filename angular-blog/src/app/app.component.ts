9adf1f7fabb104c6cba41d4809627e4e79e5d636
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2024-01-02 14:57:55 +0000

    add base files
    Aula atual: "Concentrando seu Big Card"

--- angular-blog/src/app/app.component.ts
+++ angular-blog/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SmallCardComponent } from "./components/small-card/small-card.component";
+import { BigCardComponent } from "./components/big-card/big-card.component";
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, SmallCardComponent, BigCardComponent, MenuTitleComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
---
+++ angular-blog/src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BigCardComponent } from "../../components/big-card/big-card.component";
+import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [BigCardComponent, MenuTitleComponent]
+})
+export class HomeComponent {}
