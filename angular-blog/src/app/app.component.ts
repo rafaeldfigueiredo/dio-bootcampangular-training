e94bff3dfb2be25dd912ce48136820de44686ed6
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2024-01-02 12:28:42 +0000

    reset angular-blog

--- angular-blog/src/app/app.component.ts
+++ angular-blog/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { MenuBarComponent } from "./menu-bar/menu-bar.component";
-import { MenuTitleComponent } from "./menu-title/menu-title.component";
-import { BigCardComponent } from "./big-card/big-card.component";
-import { SmallCardComponent } from "./small-card/small-card.component";
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MenuBarComponent,MenuTitleComponent,BigCardComponent,SmallCardComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'angular-blog';
+}
