55ec8d407f80310cbdba1fb3cbdabe28f764d70e
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2024-01-03 14:38:24 +0000

    add files of styling

--- angular-blog/src/app/pages/home/home.component.ts
+++ angular-blog/src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { BigCardComponent } from "../../components/big-card/big-card.component";
 import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
+import { SmallCardComponent } from "../../components/small-card/small-card.component";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [BigCardComponent, MenuTitleComponent]
+    imports: [BigCardComponent, MenuTitleComponent, SmallCardComponent, MenuBarComponent]
 })
 export class HomeComponent {}
