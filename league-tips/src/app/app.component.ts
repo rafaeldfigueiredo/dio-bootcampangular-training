4a2d1b0c9a0c4b06b912a3fb255203b8c8888bbb
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2024-01-15 05:44:26 +0000

    added gitpod relevant edits

--- league-tips/src/app/app.component.ts
+++ league-tips/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import tips from './data/tips.json';
-
-console.log(tips);
+import tipsjson from './data/tips.json';
 
 @Component({
   selector: 'app-root',
@@ -10,20 +8,6 @@ console.log(tips);
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-
-
-
 export class AppComponent {
-  list = Object.keys(tips);
-  tips = Object.keys(tips.Toplaner.tips);
-  tipsLane = Object.values(tips.Toplaner.tips);
-
-  lanebuttonPressed: boolean = false;
-  buttonPress() {
-    this.lanebuttonPressed = !this.lanebuttonPressed;
-  }
-  tipsbuttonPressed: boolean = false;
-  tipsbutton(){
-    this.tipsbuttonPressed = !this.tipsbuttonPressed;
-  }
+  tips = Object.entries(tipsjson)
 }
